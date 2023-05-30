0226c3cfaea1aebfcbfa3d36df070e13f112a0fd
JasonStephen <b1685941616@gmail.com>
2023-05-30 15:42:36 +0800

    Class Exercise

--- src/main/java/com/c6c1/C6C1Application.java
+++ src/main/java/com/c6c1/C6C1Application.java
@@ -2,7 +2,9 @@ package com.c6c1;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cache.annotation.EnableCaching;
 
+@EnableCaching   //  开启Spring Boot基于注解的缓存管理支持
 @SpringBootApplication
 public class C6C1Application {
 
--- src/test/java/com/c6c1/C6C1ApplicationTests.java
+++ src/test/java/com/c6c1/C6C1ApplicationTests.java
@@ -1,8 +1,14 @@
 package com.c6c1;
 
-import org.junit.jupiter.api.Test;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.data.redis.core.RedisTemplate;
+import org.springframework.data.redis.core.StringRedisTemplate;
+import org.springframework.test.context.junit4.SpringRunner;
 
+@RunWith(SpringRunner.class)
 @SpringBootTest
 class C6C1ApplicationTests {
 
